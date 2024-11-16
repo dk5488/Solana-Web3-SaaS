9d7d0af5bd7f51e39e232800ec3a950bf5418fe4
dk5488 <divypandey104@gmail.com>
2024-11-16 19:23:05 +0530

    worker middleware and signin

--- backend/src/middleware.ts
+++ backend/src/middleware.ts
@@ -1,6 +1,7 @@
 import { NextFunction, Request, Response } from "express";
 import jwt from "jsonwebtoken";
 const JWT_SECRET = "Divy123";
+import { WORKER_JWT_SECRET } from "./routers/workerRoutes";
 
 export function authMiddleware(req: Request,res: Response,next: NextFunction) {
 
@@ -30,3 +31,34 @@ export function authMiddleware(req: Request,res: Response,next: NextFunction) {
 
   }
 }
+
+
+export function workerMiddleware(req: Request,res: Response,next: NextFunction) {
+
+  const header = req.headers["authorization"] ?? "";
+
+  try {
+    const decrypt = jwt.verify(header, WORKER_JWT_SECRET);
+    //@ts-ignore
+    if (decrypt.userId) {
+        //@ts-ignore
+      req.userId = decrypt.userId;
+
+      return next();
+
+    } else {
+
+      return res.status(403).json({
+        message: "User Not logged in",
+      });
+
+    }
+  } catch (error) {
+
+    return res.status(403).json({
+      message: "User Not logged in please try again",
+    });
+
+  }
+}
+
--- backend/src/routers/workerRoutes.ts
+++ backend/src/routers/workerRoutes.ts
@@ -3,12 +3,52 @@ import { Router } from "express";
 import jwt from "jsonwebtoken";
 import { S3Client, GetObjectCommand, PutObjectCommand } from "@aws-sdk/client-s3";
 import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
-import { authMiddleware } from "../middleware";
+import { workerMiddleware } from "../middleware";
 
 const router = Router();
 const prismaClient = new PrismaClient();
-const JWT_SECRET = "Divy123";
+export const WORKER_JWT_SECRET = "Divy123";
 
-
+router.post("/signin",async(req,res)=>{
+    try {
+        const walletAddress = "HijWBNXaHo76YvRwQLHwwQ4RwRLdbYagjwUQyE6Any7u";
+        const existingWorker = await prismaClient.worker.findFirst({
+          where: {
+            address: walletAddress,
+          },
+        });
+    
+        if (existingWorker) {
+          const token = jwt.sign(
+            {
+              userId: existingWorker.id,
+            },
+            WORKER_JWT_SECRET 
+          );
+     
+          res.json({ token });
+        } else {
+          const worker = await prismaClient.worker.create({
+            data: {
+              address: walletAddress,
+              pending_amount:0,
+              locked_amount:0
+            },
+          });
+    
+          const token = jwt.sign(
+            {
+              userId: worker.id,
+            },
+            WORKER_JWT_SECRET
+          );
+    
+          res.json({ token });
+        }
+      } catch (error) {
+        console.error("Error during signin:", error);
+        res.status(500).json({ message: "Error during signin" });
+      }
+})
 
 export default router;
