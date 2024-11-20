477e1e6464cb4758ce0e31ec3582cdbb2faea9d4
dk5488 <divypandey104@gmail.com>
2024-11-20 21:42:18 +0530

    JWT config

--- backend/src/config.ts
+++ backend/src/config.ts
@@ -1,4 +1,4 @@
-export const JWT_SECRET = process.env.JWT_SECRET ?? "kirat123";
+export const JWT_SECRET = process.env.JWT_SECRET ?? "DivyPandey";
 export const WORKER_JWT_SECRET = JWT_SECRET + "worker";
 
 export const TOTAL_DECIMALS = 1000_000;
